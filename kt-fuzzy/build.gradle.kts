453bea0f6c38f4d66de8d8696017e4a58ac1177f
solonovamax <solonovamax@12oclockpoint.com>
2021-10-22 15:33:49 -0400

    Add subproject build.gradle.kts files
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

---
+++ kt-fuzzy/build.gradle.kts
@@ -0,0 +1,98 @@
+/*
+ * kt-fuzzy - A Kotlin library for fuzzy string matching
+ * Copyright (c) 2021 solonovamax <solonovamax@12oclockpoint.com>
+ *
+ * The file build.gradle.kts is part of kt-fuzzy
+ * Last modified on 22-10-2021 03:27 p.m.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * KT-FUZZY IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+plugins {
+    kotlin("multiplatform") apply true
+}
+
+group = "ca.solo-studios"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.languageVersion = "1.5"
+            kotlinOptions.apiVersion = "1.5"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    
+    js(BOTH) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux"    -> linuxX64("native")
+        isMingwX64           -> mingwX64("native")
+        else                 -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+    
+    sourceSets {
+        val commonMain by getting {
+            
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting {
+            
+        }
+        val jvmTest by getting {
+            
+        }
+        val jsMain by getting {
+            
+        }
+        val jsTest by getting {
+            
+        }
+        val nativeMain by getting {
+            
+        }
+        val nativeTest by getting {
+            
+        }
+    }
+}
