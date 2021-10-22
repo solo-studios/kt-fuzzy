01d0ed0c57c2c26022d4116552f118577a5aba0f
solonovamax <solonovamax@12oclockpoint.com>
2021-10-22 17:31:30 -0400

    Add OptimalStringAlignment distance
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

---
+++ kt-string-similarity/src/commonMain/kotlin/ca/solostudios/stringsimilarity/OptimalStringAlignment.kt
@@ -0,0 +1,112 @@
+/*
+ * kt-string-similarity - A library implementing different string similarity and distance measures.
+ * Copyright (c) 2015 Thibault Debatty
+ *
+ * The file OptimalStringAlignment.kt is part of kt-fuzzy
+ * Last modified on 22-10-2021 05:31 p.m.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * KT-STRING-SIMILARITY IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package ca.solostudios.stringsimilarity
+
+import ca.solostudios.stringsimilarity.interfaces.StringDistance
+import kotlin.math.min
+
+/**
+ * Implementation of the the Optimal String Alignment (sometimes called the
+ * restricted edit distance) variant of the Damerau-Levenshtein distance.
+ *
+ * The difference between the two algorithms consists in that the Optimal String
+ * Alignment algorithm computes the number of edit operations needed to make the
+ * strings equal under the condition that no substring is edited more than once,
+ * whereas Damerau-Levenshtein presents no such restriction.
+ *
+ * @author Michail Bogdanos
+ */
+public class OptimalStringAlignment : StringDistance {
+    /**
+     * Compute the distance between strings: the minimum number of operations
+     * needed to transform one string into the other (insertion, deletion,
+     * substitution of a single character, or a transposition of two adjacent
+     * characters) while no substring is edited more than once.
+     *
+     * @param s1 The first string to compare.
+     * @param s2 The second string to compare.
+     * @return the OSA distance
+     */
+    override fun distance(s1: String, s2: String): Double {
+        
+        if (s1 == s2) {
+            return 0.0
+        }
+        val n = s1.length
+        val m = s2.length
+        if (n == 0) {
+            return m.toDouble()
+        }
+        if (m == 0) {
+            return n.toDouble()
+        }
+        
+        // Create the distance matrix H[0 .. s1.length+1][0 .. s2.length+1]
+        val d = Array(n + 2) { IntArray(m + 2) }
+        
+        // initialize top row and leftmost column
+        for (i in 0 .. n) {
+            d[i][0] = i
+        }
+        for (j in 0 .. m) {
+            d[0][j] = j
+        }
+        
+        // fill the distance matrix
+        var cost: Int
+        for (i in 1 .. n) {
+            for (j in 1 .. m) {
+                
+                // if s1[i - 1] = s2[j - 1] then cost = 0, else cost = 1
+                cost = 1
+                if (s1[i - 1] == s2[j - 1]) {
+                    cost = 0
+                }
+                d[i][j] = min(
+                        d[i - 1][j - 1] + cost,  // substitution
+                        d[i][j - 1] + 1,  // insertion
+                        d[i - 1][j] + 1 // deletion
+                             )
+                
+                // transposition check
+                if (i > 1 && j > 1 && s1[i - 1] == s2[j - 2] && s1[i - 2] == s2[j - 1]) {
+                    d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)
+                }
+            }
+        }
+        return d[n][m].toDouble()
+    }
+    
+    private companion object {
+        private fun min(
+                a: Int, b: Int, c: Int
+                       ): Int {
+            return min(a, min(b, c))
+        }
+    }
+}
