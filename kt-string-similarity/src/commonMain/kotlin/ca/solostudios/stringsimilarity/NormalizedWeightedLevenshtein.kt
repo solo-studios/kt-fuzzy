9631639eae96c1569eeb17b81a49661ebfc7a05d
solonovamax <solonovamax@12oclockpoint.com>
2021-10-26 15:42:22 -0400

    Update documentation for NormalizedWeightedLevenshtein

--- kt-string-similarity/src/commonMain/kotlin/ca/solostudios/stringsimilarity/NormalizedWeightedLevenshtein.kt
+++ kt-string-similarity/src/commonMain/kotlin/ca/solostudios/stringsimilarity/NormalizedWeightedLevenshtein.kt
@@ -13,7 +13,9 @@ public class NormalizedWeightedLevenshtein(
      * Compute distance as WeightedLevenshtein(s1, s2) / max(|s1|, |s2|).
      * @param s1 The first string to compare.
      * @param s2 The second string to compare.
-     * @return The computed distance in the range [0, 1]
+     * @return If the weights always return a value between 1 and 0, then the resulting value will be between those.
+     *         The resulting value can be as *high as* the highest weight provided.
+     *         Will always be >= 0.
      */
     override fun distance(s1: String, s2: String): Double {
         if (s1 == s2) {
