ff84e44a4a50e50f2d489f0afe9afa627d5fa54c
solonovamax <solonovamax@12oclockpoint.com>
2023-08-02 01:21:10 -0400

    Remove useless comments from OptimalStringAlignmentTest
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- kt-string-similarity/src/commonTest/kotlin/ca/solostudios/stringsimilarity/edit/OptimalStringAlignmentTest.kt
+++ kt-string-similarity/src/commonTest/kotlin/ca/solostudios/stringsimilarity/edit/OptimalStringAlignmentTest.kt
@@ -3,7 +3,7 @@
  * Copyright (c) 2015-2023 solonovamax <solonovamax@12oclockpoint.com>
  *
  * The file OptimalStringAlignmentTest.kt is part of kotlin-fuzzy
- * Last modified on 01-08-2023 11:49 p.m.
+ * Last modified on 02-08-2023 01:21 a.m.
  *
  * MIT License
  *
@@ -66,19 +66,4 @@ class OptimalStringAlignmentTest : FunSpec({
             optimalStringAlignment
         )
     )
-
-    // // equality
-    // assertEquals(0.0, instance.distance("ABDCEF", "ABDCEF"), 0.0)
-    //
-    // // single operation
-    // assertEquals(1.0, instance.distance("ABDCFE", "ABDCEF"), 0.0)
-    // assertEquals(1.0, instance.distance("BBDCEF", "ABDCEF"), 0.0)
-    // assertEquals(1.0, instance.distance("BDCEF", "ABDCEF"), 0.0)
-    // assertEquals(1.0, instance.distance("ABDCEF", "ADCEF"), 0.0)
-    //
-    // // other
-    // assertEquals(3.0, instance.distance("CA", "ABC"), 0.0)
-    // assertEquals(2.0, instance.distance("BAC", "CAB"), 0.0)
-    // assertEquals(4.0, instance.distance("abcde", "awxyz"), 0.0)
-    // assertEquals(5.0, instance.distance("abcde", "vwxyz"), 0.0)
 })
